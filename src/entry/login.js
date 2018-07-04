94b28dc8d3e9518fd991e823864d1ac194c18f53
mabiao <mabiao@linghit.com>
2018-07-04 18:34:23 +0800

    开发

---
+++ src/entry/login.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from '../pages/login'
+import '../assets/js/flexible'
+import { AlertPlugin, ToastPlugin,Icon  } from 'vux'
+
+
+Vue.use(AlertPlugin)
+Vue.use(ToastPlugin)
+Vue.component('icon', Icon)
+
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+let vm  = new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App/>',
+})
---
+++ src/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <group>
+           <x-input title="title" v-model="value"></x-input>
+        </group>
+    </div>
+</template>
+
+<script>
+    import { Group, XInput  } from 'vux'
+    export default {
+        components: {
+            XInput,
+            Group
+        },
+        data () {
+            return {
+                list: [{
+                  label: 'Apple',
+                  value: '3.29'
+                }, {
+                  label: 'Banana',
+                  value: '1.04'
+                }, {
+                  label: 'Fish',
+                  value: '8.00'
+                }]
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+  @import '~vux/src/styles/reset.less';
+  @import '~vux/src/styles/1px.less';
+  @import '~vux/src/styles/close.less';
+  @import '../assets/css/reset';
+  #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+  }
+</style>
